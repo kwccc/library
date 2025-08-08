818182ce56e92dcdf00472d6daeb2a12e39838e0
Kai Cheung <kaicheunga@gmail.com>
2025-08-08 17:02:16 +0100

    Add constructor and store book in array functions

---
+++ script.js
@@ -0,0 +1,25 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  if (read) {
+    this.read = "read";
+  } else {
+    this.read = "not read yet";
+  }
+
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  };
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const book = new Book(title, author, pages, read)
+  myLibrary.push(book)
+}
