95c3406c93a0ae0bcdd33fb9501d36b37f20c5b9
Kai Cheung <kaicheunga@gmail.com>
2025-08-13 20:14:15 +0100

    Add new book form

--- script.js
+++ script.js
@@ -20,6 +20,24 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(title, author, pages, read) {
-  const book = new Book(title, author, pages, read)
-  myLibrary.push(book)
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
 }
+
+function displayBooks() {
+  myLibrary.forEach((entry) => {
+    console.log(entry.title);
+  });
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, false);
+addBookToLibrary("1984", "George Orwell", 328, true);
+addBookToLibrary("A Game of Thrones", "G.R.R Martin", 694, true);
+addBookToLibrary("Pride and Prejudice", "Jane Austen", 448, false);
+
+const button = document.querySelector("#new-book-button");
+
+button.addEventListener("click", (e) => {
+  // make form visible
+  document.querySelector(".form-popup").style.display = "block";
+});
